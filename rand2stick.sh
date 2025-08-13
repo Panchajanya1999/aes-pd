736c36b8ce489885845860ff5fb418fa5142ec81
Panchajanya1999 <kernel@panchajanya.dev>
2025-08-13 10:59:58 +0530

    rand2stick: Insert Apache License on top
    Signed-off-by: Panchajanya1999 <kernel@panchajanya.dev>

--- rand2stick.sh
+++ rand2stick.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
+
 # ------------------------------------------------------------------
+#
+#    Copyright [2025] [Panchajanya1999]
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# ------------------------------------------------------------------
+#
 # rand2stick.sh  —  Fill a block device with AES-256-CTR–encrypted
 # random bytes, leaving 1GB at the end, write a memory dump script
 # to the reserved space, and verify.
